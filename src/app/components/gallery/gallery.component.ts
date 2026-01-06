3fad87aaac8b120219dc4403a4c3bf7d891de933
DionisKarpov <denis@orientedsoft.com>
2026-01-06 19:12:26 +0200

    Update gallery component

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  imports: [CommonModule]
 })
 export class GalleryComponent {
+  selectedImage: string | null = null;
 
-  constructor() {}
+  openImage(src: string) {
+    this.selectedImage = src;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeImage() {
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
 }
